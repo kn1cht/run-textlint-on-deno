984eae42e2653bfbf1cdcff38dd9db12f813185d
kn1cht <16277200+kn1cht@users.noreply.github.com>
2022-12-16 23:28:22 +0900

    add ng-word

--- textlint.ts
+++ textlint.ts
@@ -1,5 +1,6 @@
 import { TextLintEngine } from "npm:textlint";
 import _ from "npm:textlint-rule-preset-ja-technical-writing";
+import _ from "npm:textlint-rule-ng-word";
 const engine = new TextLintEngine();
 const results = await engine.executeOnFiles(Deno.args);
 if (engine.isErrorResults(results)) console.log(engine.formatResults(results));
