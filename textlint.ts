d57f7e25f149ecdb6209a98dfd597971cd8a0032
kn1cht <16277200+kn1cht@users.noreply.github.com>
2022-12-16 23:42:53 +0900

    add plugin-latex

--- textlint.ts
+++ textlint.ts
@@ -1,6 +1,7 @@
 import { TextLintEngine } from "npm:textlint";
 import _ from "npm:textlint-rule-preset-ja-technical-writing";
 import _ from "npm:textlint-rule-ng-word";
+import _ from "npm:textlint-plugin-latex2e";
 const engine = new TextLintEngine();
 const results = await engine.executeOnFiles(Deno.args);
 if (engine.isErrorResults(results)) console.log(engine.formatResults(results));
